4e3d8d276e003cfdb8a243c44428a27b210b0ec1
Mantas <syenergyy@gmail.com>
2019-08-22 21:27:24 +0300

    Add files via upload

---
+++ PH_Fixes_ShortcutsPHS/PH_Fixes_ShortcutsPHS.cs
@@ -0,0 +1,22 @@
+﻿using Harmony;
+using System.IO;
+
+namespace PH_Fixes {
+    public class PH_Fixes_ShortcutsPHS {
+        //    errorfix    //
+        [HarmonyPatch(typeof(ShortcutsPHS.BaseSettingsGo), "GetCusFemListFile")]
+        class Patch_ShortcutsPHS_BaseSettingsGo_GetCusFemListFile {
+            static bool Prefix(ShortcutsPHS.BaseSettingsGo __instance) {
+                Traverse traverse = Traverse.Create(__instance);
+
+                FileInfo file = new FileInfo(@"Plugins/ShortcutsPHS/Settings/" + traverse.Field("cusFemListFileName").GetValue<string>());
+                if (!file.Exists) {
+                    traverse.Method("resetCusFemList").GetValue();
+                    return false;
+                }
+
+                return true;
+            }
+        }
+    }
+}
