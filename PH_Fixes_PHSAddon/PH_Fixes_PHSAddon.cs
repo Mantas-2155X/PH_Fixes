b49fecc9a475637ac4dd412c6254002fea6711f3
Mantas <syenergyy@gmail.com>
2019-08-22 21:25:14 +0300

    Add files via upload

---
+++ PH_Fixes_PHSAddon/PH_Fixes_PHSAddon.cs
@@ -0,0 +1,18 @@
+﻿using Harmony;
+
+namespace PH_Fixes {
+    public class PH_Fixes_PHSAddon {
+        //    errorfix    //
+        [HarmonyPatch(typeof(PHSAddon.MMD_VmdCameraLoad), "getAnimationFrame")]
+        class Patch_PHSAddon_MMD_VmdCameraLoad_getAnimationFrame {
+            static bool Prefix(PHSAddon.MMD_VmdCameraLoad __instance, float __result) {
+                if (__instance.hsvmdanimationController == null) {
+                    __result = 0f;
+                    return false;
+                }
+
+                return true;
+            }
+        }
+    }
+}
