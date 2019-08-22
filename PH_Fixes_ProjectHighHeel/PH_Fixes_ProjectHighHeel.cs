e3ca548f5e2b117fe37f256fbbb2000cfa6ba6ff
Mantas <syenergyy@gmail.com>
2019-08-22 18:40:31 +0300

    Add files via upload

---
+++ PH_Fixes_ProjectHighHeel/PH_Fixes_ProjectHighHeel.cs
@@ -0,0 +1,35 @@
+﻿using Harmony;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Collections.Generic;
+
+namespace PH_Fixes {
+    public class PH_Fixes_ProjectHighHeel {
+        //     errorfix     // BAD, NEED TO INSERT NULLCHECK INSTEAD!! //
+        [HarmonyPatch(typeof(ProjectHighHeel.HighHeelRuntime), "UIinputs")]
+        class Patch_HighHeelRuntime_UIinputs {
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
+                var il = instructions.ToList();
+
+                var targetIndex = il.FindIndex(instruction => instruction.opcode == OpCodes.Ret);
+
+                if (targetIndex > 0) {
+                    il[targetIndex - 1].opcode = OpCodes.Nop;
+                    il[targetIndex - 2].opcode = OpCodes.Nop;
+                    il[targetIndex - 3].opcode = OpCodes.Nop;
+                    il[targetIndex - 4].opcode = OpCodes.Nop;
+                    il[targetIndex - 5].opcode = OpCodes.Nop;
+                    il[targetIndex - 6].opcode = OpCodes.Nop;
+                    il[targetIndex - 7].opcode = OpCodes.Nop;
+                    il[targetIndex - 8].opcode = OpCodes.Nop;
+                    il[targetIndex - 9].opcode = OpCodes.Nop;
+                    il[targetIndex - 10].opcode = OpCodes.Nop;
+                    il[targetIndex - 11].opcode = OpCodes.Nop;
+                    il[targetIndex - 12].opcode = OpCodes.Nop;
+                }
+
+                return il;
+            }
+        }
+    }
+}
